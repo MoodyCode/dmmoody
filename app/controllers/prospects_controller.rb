cc4f42acf92f2b2968135022821693ba84ae6407
Duane M. Moody <dmmoody@gmail.com>
2015-09-12 19:23:49 -0700

    add sendgrid mailer

---
+++ app/controllers/prospects_controller.rb
@@ -0,0 +1,27 @@
+class ProspectsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @prospect = Prospect.new
+  end
+
+  def create
+    @prospect = Prospect.new(prospect_params)
+    if @prospect.save
+      UserNotifier.send_prospect_confirm_email(@prospect).deliver
+      redirect_to :root
+    else
+      redirect_to new_prospects_path
+    end
+  end
+
+private
+  def prospect_params
+    params.require(:prospect). permit(:name, :phone, :email, :description, :timetable, :referral, :budget)
+  end
+end
--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -7,4 +7,10 @@ class UserNotifier < ApplicationMailer
     mail( :to => @user.email,
     :subject => 'Thanks for signing up for our amazing app' )
   end
+
+  def send_prospect_confirm_email(prospect)
+    @prospect = prospect
+    mail( :to => @prospect.email,
+      :subject => "Thanks, your request for proposal was submitted")
+  end
 end
